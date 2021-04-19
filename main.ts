7d1b1125c01184a299317494457d47280e270b5d
ShawnReardon <shawnreardon1@gmail.com>
2021-04-19 15:03:08 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+let mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+mySprite.setStayInScreen(true)
+mySprite.setBounceOnWall(true)
+let velocity = 25
+mySprite.setVelocity(0, velocity)
+forever(function () {
+    if (mySprite.isHittingTile(CollisionDirection.Top)) {
+        velocity = velocity * -1
+        mySprite.setVelocity(0, velocity)
+    } else if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        velocity = 500
+        mySprite.setVelocity(0, velocity)
+    }
+})
